391e68a1246413bb823860991d90d265949bd7bb
ganovelli <fabio.ganovelli@gmail.com>
2025-01-29 21:05:05 +0100

    versione fatta a lezione
    versione fatta a lezione

--- src/code_00_raytracer/main.cpp
+++ src/code_00_raytracer/main.cpp
@@ -25,7 +25,7 @@ struct image {
 		data[(j * w + i) * 3 + 2] = (unsigned char)b;
 	}
 
-	void save(char* filename) {
+	void save(const char* filename) {
 		ofstream f;
 		f.open(filename);
 		f << "P3\n";
@@ -40,6 +40,7 @@ struct image {
 };
 
 struct p3 {
+	p3() {}
 	p3(float x, float y, float z):x(x),y(y),z(z){}
 	float x, y, z;
 
@@ -55,15 +56,18 @@ struct ray {
 };
 
 struct sphere {
-	sphere(p3 center, float radius):center(center), radius(radius) {}
+	
+	sphere(p3 center, float radius, p3 color ):center(center), radius(radius),color(color) {}
 
-	p3 center;
+	p3 center,color;
 	float radius;
 };
 
-
+#define BIGT 1000
 struct hit_info {
-	hit_info(p3 p, p3 n,bool hit):p(p), n( n),hit(hit){}
+	hit_info():t(BIGT),hit(false){}
+	hit_info(float t, p3 p, p3 n,bool hit):t(t),p(p), n( n),hit(hit){}
+	float t;
 	p3 p, n;
 	bool hit;
 };
@@ -77,16 +81,18 @@ hit_info hit_sphere(ray r, sphere s) {
 	float delta = B * B - 4 * A * C;
 
 	if (delta < 0)
-		return hit_info(p3(0,0,0),p3(0,0,0),false);
-
+		return hit_info();
+	
 	float t1 = (-B - sqrt(delta)) / (2 * A);
 	float t2 = (-B + sqrt(delta)) / (2 * A);
 
 	float t_min = min(t1, t2);
+	if (t_min < 0)
+		t_min = max(t1, t2);
 
 	p3 p = r.orig + r.dir * t_min;
 	p3 n = p - s.center;
-	return hit_info(p, n,true);
+	return hit_info(t_min,p, n,t_min > 0.f);
 
 }
 
@@ -96,26 +102,53 @@ void main(int args, char** argv) {
 	image a(sx, sy);
 
 	p3 l(2, 2, 0);
-	sphere s(p3(0, 0, -3), 1.f);
+	
+
+	std::vector<sphere> scene;
+	scene.push_back(sphere(p3(0, 0, -3), 1.f ,p3(255,0,0 )));
+	scene.push_back(sphere(p3(0, 2, -5), 0.5f,p3(0, 0, 255)));
+	scene.push_back(sphere(p3(0.5, 0.5, -1.5), 0.3f, p3(0, 255, 0)));
 
 	for (unsigned int i = 0; i < a.w; ++i)
 		for (unsigned int j = 0; j < a.h; ++j)
-			a.set_pixel(i, j, 0, 0, 0);
+			a.set_pixel(i, j, 100, 100, 100);
 
 	for(unsigned int i=0; i < a.w; ++i)
 		for (unsigned int j = 0; j < a.h; ++j) {
 			ray r(p3(0, 0, 0), p3(-1 + i / float(a.w) * 2, -1 + j / float(a.h) * 2, -1));
 
-			hit_info hi = hit_sphere(r, s);
-			if (hi.hit) {
-				p3 L = l - hi.p;
-				L = L *(1.f/ sqrt(L.dot(L)));
-				p3 N = hi.n;
-				N = N * (1.f / sqrt(N.dot(N)));
+			hit_info hit_closest;
 
-				float cosalpha = max(0.f, L.dot(N));
+			for (unsigned int is = 0; is < scene.size(); ++is) {
 
-				a.set_pixel(i, j, int(255*cosalpha), 0, 0);
+				sphere& s = scene[is];
+				hit_info hi = hit_sphere(r, s);
+
+				if (hi.t < hit_closest.t) {
+
+
+					hit_closest = hi;
+					p3 L = l - hi.p;
+					L = L * (1.f / sqrt(L.dot(L)));
+					
+					ray shadow_ray(hi.p+L*0.000001, L);
+					unsigned int iss;
+					for( iss =0 ; iss < scene.size(); ++iss)
+						if(hit_sphere(shadow_ray,scene[iss]).hit) 
+							break;
+					
+					p3 col(0, 0, 0);
+					if (iss == scene.size())
+					{
+						p3 N = hi.n;
+						N = N * (1.f / sqrt(N.dot(N)));
+
+						float cosalpha = max(0.f, L.dot(N));
+
+						col = s.color * cosalpha;
+					}
+					a.set_pixel(i, j, col.x, col.y, col.z);
+				}
 			}
 		}
 
